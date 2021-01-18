451c17c87d6be8d3f4c02871bc7e7125f211c9d7
Victor Assis <contato@victorassis.com.br>
2021-01-18 14:24:08 -0300

    install next-translate

--- apollo-with-translate/pages/mutation-only.js
+++ apollo-with-translate/pages/mutation-only.js
@@ -2,15 +2,20 @@ import App from '../components/App'
 import InfoBox from '../components/InfoBox'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
+import useTranslation from 'next-translate/useTranslation'
 
-const MutationOnlyPage = (props) => (
-  <App>
-    <Header />
-    <InfoBox>
-      ℹ️ That is a page only with a mutation
-    </InfoBox>
-    <Submit />
-  </App>
-)
+const MutationOnlyPage = () => {
+  const { t } = useTranslation('common')
+  return (
+    <App>
+      <Header />
+      <InfoBox>
+        <h1>{t('titleMutation')}</h1>
+        ℹ️ That is a page only with a mutation
+      </InfoBox>
+      <Submit />
+    </App>
+  )
+}
 
 export default MutationOnlyPage
